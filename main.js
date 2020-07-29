53bda12268fac6d96fabc2e063cd2edbf04b3af5
Davids-web <gdavidmanoj@gmail.com>
2020-07-30 00:34:05 +0530

    a

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var request=new XMLHttpRequest();
 request.overrideMimeType('json');
 request.open("GET",file,true);
 request.onreadystatechange=function(){
-	if (this.readyState==4 && this.status=='200') {
+	if (this.readyState==4 && this.status==200) {
 		callback(request.responseText);
 		
 	}
@@ -14,30 +14,47 @@ getdata('data.json', function(text)
 	{
 	var data=JSON.parse(text);
 	console.log(data)
-	profiledata(data.profile);}
+	aprofile(data.profilea);
+	bprofile(data.profileb);}
 	);
 
-function profiledata(card)
+function aprofile(card)
 {
-	 var pdata=document.querySelector('.card2');
- var heading=document.createElement('h1');
- heading.textContent="DoMo Profile";
+ var pdata=document.querySelector('.card-left');
+ var heading=document.createElement('h2');
+ heading.textContent="Sharon Profile";
  pdata.append(heading)
  var pic=document.createElement('img')
- pic.src="andrea3.jpg"
+ pic.src="sharon.jpg"
  pic.classList.add('col-4')
  pdata.appendChild(pic)
- var name=document.createElement('h2')
+ var name=document.createElement('h3')
  name.textContent=card.name;
  pdata.appendChild(name)
-  var mail=document.createElement('h2')
-    mail.textContent=card.email;
-    pdata.appendChild(mail)
-    var phone=document.createElement('h2')
-    phone.textContent=card.address;
-    pdata.appendChild(phone)
-    var shop=document.createElement('h2')
-    shop.textContent=card.company;
-    pdata.appendChild(shop)
+ var mail=document.createElement('h3')
+ mail.textContent=card.email;
+ pdata.appendChild(mail)
+ var phone=document.createElement('h3')
+ phone.textContent=card.add;
+ pdata.appendChild(phone)
+ var prof=document.createElement('h3')
+ prof.textContent=card.company;
+ pdata.appendChild(prof)
+   
+}
+function bprofile(bcard){
+	var pdata2=document.querySelector('.card-right');
+	var heading2=document.createElement('h2');
+	heading2.textContent='Samantha Profile';
+	pdata2.appendChild(heading2)
+	var pic=document.createElement('img');
+	pic.src='samantha.jpg'
+	pic.classList.add('col-4')
+	pdata2.appendChild(pic)
+	for (var i in bcard){
+		var dlop=document.createElement('h3');
+		dlop.textContent=bcard[i];
+		pdata2.appendChild(dlop)
+	}
 
 }
